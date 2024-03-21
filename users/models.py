93404a1bd416a63adfcb41ec43c365b0287536ae
Artem-Ter <Artem-Ter@users.noreply.github.com>
2024-03-21 17:56:19 +0000

    P2U01: project settings

--- users/models.py
+++ users/models.py
@@ -1,6 +1,7 @@
-from api.validators import MODELS_RESTRICTIONS
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+
+from api.validators import MODELS_RESTRICTIONS
 
 
 # Create your models here.
