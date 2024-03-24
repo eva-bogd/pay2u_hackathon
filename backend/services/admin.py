7199ddaf4ea17886f9804eed45085371cbfd83c9
Artem Tereschenko <altereschenko@gmail.com>
2024-03-23 23:16:19 -0300

    P2U02: add admin for services and users add return to UserTariff __str__

--- backend/services/admin.py
+++ backend/services/admin.py
@@ -1,5 +1,39 @@
 from django.contrib import admin
 
+from .models import Subscription, Tariff, Transaction, UserTariff
 
-class ServiceAdmin(admin.ModelAdmin):
-    pass
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    """Admin for subscriptions"""
+    list_display = (
+        'pk',
+        'name',
+    )
+    ordering = ('name',)
+    search_fields = ('name',)
+
+
+@admin.register(Tariff)
+class TariffAdmin(admin.ModelAdmin):
+    """Admin for tarrifs"""
+    list_display = (
+        'pk',
+        'subscription',
+        'name',
+        'duration',
+        'price',
+        'cashback',
+        'is_direct',
+    )
+    list_filter = (
+        'subscription',
+        'duration',
+        'is_direct',
+    )
+    search_fields = ('name',)
+    ordering = ('subscription',)
+
+
+admin.site.register(UserTariff)
+admin.site.register(Transaction)
