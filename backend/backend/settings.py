d34b1286088d543cdc81612cdff6c543fec0c82b
Евгения Богданова <ev002@list.ru>
2024-03-30 20:43:53 +0700

    P2U007: fix flake8-

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'drf_yasg',  # swagger
+    'drf_yasg',
     'rest_framework',
     'users.apps.UsersConfig',
     'services.apps.ServicesConfig',
